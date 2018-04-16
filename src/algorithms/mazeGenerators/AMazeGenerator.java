0c85cbb147578e16b7c3105cd68fb46833ea1e61
J-morag <38430800+J-morag@users.noreply.github.com>
2018-04-16 19:05:33 +0300

    Created Project

---
+++ src/algorithms/mazeGenerators/AMazeGenerator.java
@@ -0,0 +1,46 @@
+package algorithms.mazeGenerators;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public abstract class AMazeGenerator implements IMazeGenerator
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public AMazeGenerator(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public Maze generate() {
+		// TODO implement me
+		return null;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public int measureAlgorithmTimeMillis() {
+		// TODO implement me
+		return 0;
+	}
+
+}
+
